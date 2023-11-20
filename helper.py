b82b87be81eef8cc5a8445c60cacee101409a73b
MrCsabaToth <csaba.toth.us@gmail.com>
2023-11-19 23:57:05 -0800

    Extract string handler functions into separate file (helper)

---
+++ helper.py
@@ -0,0 +1,22 @@
+from cohere.client import Chat
+
+
+def insert_substring(source_str: str, insert_str: str, pos: int) -> str:
+    return source_str[:pos] + insert_str + source_str[pos:]
+
+
+def mark_citations(kind: str, index: int, response: Chat) -> str:
+    doc_map = dict()
+    for idx, doc in enumerate(response.documents or []):
+        if doc["id"] not in doc_map:
+            doc_map[doc["id"]] = "#" + f"{kind}_ref_{index + 1}_{idx + 1}"
+
+    txt = response.text or "N/A"
+    for cit in (response.citations or [])[::-1]:
+        doc_anchor = doc_map[cit["document_ids"][0]]
+        cit_idxs = doc_anchor.split("_")
+        cit_sup = f"{cit_idxs[-2]}.{cit_idxs[-1]}"
+        txt = insert_substring(txt, f"<sup>{cit_sup}</sup></a>", cit["end"])
+        txt = insert_substring(txt, f"<a href='{doc_anchor}' target='_self'>", cit["start"])
+
+    return txt  # .replace("\n\n", "\n").replace("\n\n", "\n").replace("\n\n", "\n")
--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ import weaviate
 
 from cohere.client import Chat
 from config import defaults, boundaries
+from helper import mark_citations
 from langchain.embeddings import CohereEmbeddings
 from langchain.llms import Cohere
 from langchain.prompts.chat import (
@@ -300,27 +301,6 @@ def generate_response_with_rag_fusion(query: str) -> tuple[Chat, Chat]:
     reranked_results = rerank_and_fuse_documents(document_sets)
     # Step 4: Prepare and executing final RAG calls (a grounded and a web)
     return final_rag_operations(query, reranked_results)
-
-
-def insert_substring(source_str: str, insert_str: str, pos: int) -> str:
-    return source_str[:pos] + insert_str + source_str[pos:]
-
-
-def mark_citations(kind: str, index: int, response) -> str:
-    doc_map = dict()
-    for idx, doc in enumerate(response.documents or []):
-        if doc["id"] not in doc_map:
-            doc_map[doc["id"]] = "#" + f"{kind}_ref_{index + 1}_{idx + 1}"
-
-    txt = response.text or "N/A"
-    for cit in (response.citations or [])[::-1]:
-        doc_anchor = doc_map[cit["document_ids"][0]]
-        cit_idxs = doc_anchor.split("_")
-        cit_sup = f"{cit_idxs[-2]}.{cit_idxs[-1]}"
-        txt = insert_substring(txt, f"<sup>{cit_sup}</sup></a>", cit["end"])
-        txt = insert_substring(txt, f"<a href='{doc_anchor}' target='_self'>", cit["start"])
-
-    return txt  # .replace("\n\n", "\n").replace("\n\n", "\n").replace("\n\n", "\n")
 
 
 clear_button = st.sidebar.button("Clear Conversation", key="clear")
